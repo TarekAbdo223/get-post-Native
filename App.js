c4f21c3425af110e66d059982a27b39f0a21a6e1
Tarek Abdo <tarekk.abdo22@gmail.com>
2024-04-27 23:04:51 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,165 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  ActivityIndicator,
+  Button,
+  TextInput,
+} from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    );
+    const data = await response.json();
+    setPosts(data);
+    setIsLoading(false);
+  };
+
+  const handleRefresh = () => {
+    setRefresh(true);
+    fetchData(20);
+    setRefresh(false);
+  };
+
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+    });
+    const newPost = await response.json();
+    setPostTitle("");
+    setPostBody("");
+    setIsPosting(false);
+    setPosts([newPost, ...posts]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="0000ff" />
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Post Title"
+            style={styles.input}
+            onChangeText={(text) => setPostTitle(text)}
+            value={postTitle}
+          />
+          <TextInput
+            placeholder="Post Body"
+            style={styles.input}
+            onChangeText={(text) => setPostBody(text)}
+            value={postBody}
+          />
+          <Button
+            title={isPosting ? "Adding..." : "Add Post"}
+            onPress={addPost}
+            disabled={isPosting}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            refreshing={refresh}
+            onRefresh={handleRefresh}
+            ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+            ListEmptyComponent={<Text>No Posts Found</Text>}
+            ListHeaderComponent={
+              <Text style={styles.headerText}>Post list</Text>
+            }
+            ListFooterComponent={
+              <Text style={styles.footerText}>End of list</Text>
+            }
+            data={posts}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.card}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.body}>{item.body}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  title: {
+    fontSize: 30,
+  },
+  body: {
+    color: "#666666",
+    fontSize: 16,
+  },
+  headerText: { fontSize: 24, marginBottom: 12, textAlign: "center" },
+  footerText: { fontSize: 24, marginTop: 12, textAlign: "center" },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 8,
   },
 });
